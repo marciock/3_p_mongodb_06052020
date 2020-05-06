ce639f84724deadd2520d77da88721c886c83c37
marciock <marciockalves@gmail.com>
2020-05-06 14:13:46 -0300

    aula 06052020

---
+++ core/route.php
@@ -0,0 +1,14 @@
+<?php
+
+$route[]=['/','dashController'];
+$route[]=['cadastro','cadastroController'];
+$route[]=['editar','editarController'];
+$route[]=['inserir','insertController'];
+$route[]=['atualizar','uploadController'];
+
+
+
+
+
+
+//return $route;
---
+++ views/dash.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">CPF</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Email</th>
+        <th scope="col"> </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td><a href="editar" class="bnt">Editar</a></td>
+        </tr>
+    
+    </tbody>
+    </table>
+</div>
